0decfc09a50f92dd6db2567bc44ade8806cccd77
VentCat <2546650292@qq.com>
2024-02-25 15:42:45 +0800

    init

---
+++ constants.py
@@ -0,0 +1,24 @@
+from enum import IntEnum, unique
+
+
+@unique
+class ChatType(IntEnum):
+    # UnKnown = 0  # 未知, 即未设置
+    TIGER_BOT = 1  # TigerBot
+    CHATGPT = 2  # ChatGPT
+    XINGHUO_WEB = 3  # 讯飞星火
+    CHATGLM = 4  # ChatGLM
+    BardAssistant = 5  # Google Bard
+    ZhiPu = 6  # ZhiPu
+
+    @staticmethod
+    def is_in_chat_types(chat_type: int) -> bool:
+        if chat_type in [ChatType.TIGER_BOT.value, ChatType.CHATGPT.value,
+                         ChatType.XINGHUO_WEB.value, ChatType.CHATGLM.value,
+                         ChatType.BardAssistant.value]:
+            return True
+        return False
+
+    @staticmethod
+    def help_hint() -> str:
+        return str({member.value: member.name for member in ChatType}).replace('{', '').replace('}', '')
